e746b0dd07a2ae879d59580f951161ecbeae36aa
Igor Martins <iguinhoigor4@gmail.com>
2023-12-19 16:27:11 -0300

    feat: IT'S TIIIIIIMEEEE - atualização de Natal

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -69,7 +69,7 @@ function exibeLinha(posicao) {
 
 // Configurações dos ícones de marcadores das paradas de ônibus
 const stationIcon = L.icon({
-  iconUrl: "./src/assets/marker.svg",
+  iconUrl: "./src/assets/marker_natal.svg",
   iconSize: [30, 30],
   iconAnchor: [20, 20],
   popupAnchor: [-5, -18]
