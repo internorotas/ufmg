8c40227dcdbd145674083d1a0ab7857d598b3cd9
Igor Martins <iguinhoigor4@gmail.com>
2025-04-15 13:18:33 -0300

    feat: update configuration and add deployment scripts for GitHub Pages

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
+  output: 'export'
 };
 
 export default nextConfig;
