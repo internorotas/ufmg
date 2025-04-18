cf639eebee59d77ccc6fd9b869497656a92775b9
Igor Martins <iguinhoigor4@gmail.com>
2025-04-18 11:55:22 -0300

    fix: update footer copyright year to 2025

--- front/src/components/Mapa/Mapa.tsx
+++ front/src/components/Mapa/Mapa.tsx
@@ -15,21 +15,34 @@ const coresLinhas = [
   "#D2691E",
 ];
 
+function CustomZoomControl() {
+  const map = useMap();
+
+  // Remove o controle de zoom padrão
+  map.zoomControl.remove();
+
+  // Adiciona o controle de zoom no lado direito
+  L.control.zoom({ position: "topright" }).addTo(map);
+
+  return null;
+}
+
 export default function Mapa() {
   return (
-    <div className="w-full h-[100vh] relative">s
+    <div className="w-full h-[100%] relative">
       <MapContainer
-      center={[-19.8706, -43.9678]}
-      zoom={15}
-      scrollWheelZoom={true}
-      style={{ height: "100%", width: "100%" }}
+        center={[-19.8706, -43.9678]}
+        zoom={15}
+        scrollWheelZoom={true}
+        style={{ height: "100%", width: "100%" }}
+        zoomControl={false} // Desativa o controle de zoom padrão
       >
-      <TileLayer
-        attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        <TileLayer
+          attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <CustomZoomControl />
       </MapContainer>
     </div>
-   
   );
 }
