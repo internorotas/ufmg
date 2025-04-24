feb75ba40a1814188c7daa4024f91b1da33f515c
Igor Martins <iguinhoigor4@gmail.com>
2025-04-24 10:51:43 -0300

    feat: adicionar suporte ao plugin vite-plugin-gh-pages para deploy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ghPages()],
+  base: '/ufmg/',
 });
