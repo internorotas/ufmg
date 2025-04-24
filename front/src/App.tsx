9d41981e079ee9d1c1903fc18875c2770b573978
Igor Martins <iguinhoigor4@gmail.com>
2025-04-24 11:37:54 -0300

    feat: reorganizar estrutura do componente App para incluir Sidebar fixa sobre o Mapa

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,19 @@
-import Mapa from "./components/Mapa";
 import "./index.css";
+import Mapa from "./components/Mapa/Mapa";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
-      <Mapa />
+    <div className="relative h-screen w-screen">
+      {/* Sidebar fixa sobre o mapa */}
+      <div className="fixed top-0 left-0 z-50">
+        <Sidebar />
+      </div>
+
+      {/* Mapa ocupando toda a tela */}
+      <div className="absolute top-0 left-0 z-10 h-full w-full">
+        <Mapa />
+      </div>
     </div>
   );
 }
