bfa1c552fe849097d86d76b08a4a3c923ba59f8e
Igor Martins <iguinhoigor4@gmail.com>
2025-04-28 17:32:04 -0300

    feat: adicionar componente Linha com links de navegação

---
+++ front/src/components/Linha/Linha.tsx
@@ -0,0 +1,15 @@
+export default function Linha() {
+  return (
+    <ul className="space-y-4">
+      <li>
+        <a
+          href="#section1"
+          className="block hover:text-gray-300"
+          aria-label="Ir para a seção 1"
+        >
+          Section 1
+        </a>
+      </li>
+    </ul>
+  );
+}
