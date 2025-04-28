7e246fb7ba2fa3fb6b4a5d234f0d0b76e9aef761
Igor Martins <iguinhoigor4@gmail.com>
2025-04-28 17:26:29 -0300

    feat: adicionar fonte Poppins ao projeto e atualizar estilos

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,16 @@
 import Mapa from "./components/Mapa/Mapa";
 import Sidebar from "./components/Sidebar/Sidebar";
 
+import '@fontsource/poppins/100.css';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
+
 import "./index.css";
 
 export default function App() {
