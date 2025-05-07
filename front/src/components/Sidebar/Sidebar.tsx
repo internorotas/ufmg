39fbae7b94f2bd8ef9f6460c37a405b9d7edb8d1
Igor Martins <iguinhoigor4@gmail.com>
2025-05-07 00:10:55 -0300

    feat: atualizar layout do componente Sidebar e remover seções desnecessárias

--- front/src/components/Sidebar/Sidebar.tsx
+++ front/src/components/Sidebar/Sidebar.tsx
@@ -2,37 +2,22 @@ import Linha from "../Linha/Linha";
 
 export default function Sidebar() {
   return (
-    <div className="fixed top-4 left-4 h-[calc(100%-2rem)] w-[25%] bg-internoRotas-preto-carvao text-white shadow-lg z-50 rounded-lg flex flex-col">
-      {/* Header */}
-      <header className="p-4 flex justify-center bg-internoRotas-azul-eletrico rounded-l-lg rounded-r-lg">
-        <img
-          src="src/assets/logo-horizontal-transparente.svg"
-          alt="Logo do Interno Rotas"
-          width={150}
-          height={60}
-        />
-      </header>
-
+    <div className="fixed top-20 left-4 h-[calc(100%-10rem)] w-[30%] bg-internoRotas-preto-carvao text-white shadow-lg z-50 rounded-4xl flex flex-col overflow-hidden">
       {/* Navigation */}
       <nav className="py-4 flex-1 overflow-y-auto">
         <div>
-          <Linha key={1} codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
+          <Linha  codigo={1} />
         </div>
       </nav>
-
-      {/* Footer */}
-      <footer className="p-4 border-t border-gray-700 text-center">
-        <a
-          href="https://github.com/igormartins4"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-internoRotas-bege-areia"
-        >
-          <p className="text-sm font-[600] text-internoRotas-bege-areia">
-            Desenvolvido com 💙 por Igor Martins
-          </p>
-        </a>
-      </footer>
     </div>
   );
 }
