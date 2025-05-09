9fcffa4662dd898a626512457e292ae3084d9336
Igor Martins <iguinhoigor4@gmail.com>
2025-05-09 00:59:58 -0300

    feat: adicionar componente Footer com informações de autoria

---
+++ front/src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-4 left-4 w-full z-50 bg-internoRotas-preto-carvao text-internoRotas-bege-areia p-4 rounded-2xl shadow-lg text-center">
+      <a
+        href="https://github.com/igormartins4"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-internoRotas-bege-areia"
+      >
+        <p className="text-sm font-semibold">
+          Desenvolvido com 💙 por Igor Martins
+        </p>
+      </a>
+    </footer>
+  );
+}
