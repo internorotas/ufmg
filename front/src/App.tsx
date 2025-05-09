a015ee3bd1675d6544d12625ece4b9b148421da9
Igor Martins <igor44@ufmg.br>
2025-05-09 00:59:53 -0300

    feat: reorganizar estrutura do componente App e remover código comentado

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,44 +1,41 @@
-// App.tsx
 import "./index.css";
+
 import Mapa from "./components/Mapa/Mapa";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="relative h-screen w-screen">
-      {/* Header flutuante */}
-      <header className="fixed top-4 left-4 w-[30%] z-50 bg-internoRotas-azul-eletrico rounded-2xl p-2 shadow-lg text-center p-4 flex justify-center items-center">
-        <img
-          src="src/assets/logo-horizontal-transparente.svg"
-          alt="Logo do Interno Rotas"
-          width={150}
-          height={60}
-        />
-      </header>
-
-      {/* Sidebar fixa */}
-      <Sidebar />
-
-      {/* Mapa ocupando toda a tela */}
-      <div className="absolute top-0 left-0 z-10 h-full w-full">
+    <div>
+      <div className="absolute inset-0 z-0">
         <Mapa />
       </div>
+      <div className="z-10 items-center justify-center">
+        <Header />
 
-      {/* Footer fixo na parte inferior */}
-      <footer className="fixed bottom-4 left-4 w-[30%] z-50 bg-internoRotas-preto-carvao text-internoRotas-bege-areia p-4 rounded-2xl shadow-lg text-center">
-        <a
-          href="https://github.com/igormartins4"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-internoRotas-bege-areia"
-        >
-          <p className="text-sm font-semibold">
-            Desenvolvido com 💙 por Igor Martins
-          </p>
-        </a>
-      </footer>
+        <Sidebar />
+
+        <Footer />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* 
+  <div className="relative z-10 flex flex-col flex-grow w-full">
+  <div className="w-full">
+    <Header />
+  </div>
+
+  <div className="flex-grow flex items-center justify-center">
+    <Sidebar />
+  </div>
+
+  <Footer />
+</div>; 
+*/
+}
