e19aeee76b8f20569350d45023b04c9b938f4223
Igor Martins <iguinhoigor4@gmail.com>
2025-05-09 01:00:41 -0300

    feat: corrigir propriedade do componente Linha para numeroLinha e ajustar largura da Sidebar

--- front/src/components/Sidebar/Sidebar.tsx
+++ front/src/components/Sidebar/Sidebar.tsx
@@ -2,20 +2,20 @@ import Linha from "../Linha/Linha";
 
 export default function Sidebar() {
   return (
-    <div className="fixed top-20 left-4 h-[calc(100%-10rem)] w-[30%] bg-internoRotas-preto-carvao text-white shadow-lg z-50 rounded-4xl flex flex-col overflow-hidden">
+    <div className="fixed top-20 left-4 h-[calc(100%-10rem)] w-full bg-internoRotas-preto-carvao text-white shadow-lg z-50 rounded-4xl flex flex-col overflow-hidden">
       {/* Navigation */}
       <nav className="py-4 flex-1 overflow-y-auto">
         <div>
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
-          <Linha  codigo={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
+          <Linha numeroLinha={1} />
         </div>
       </nav>
     </div>
