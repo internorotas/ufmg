3aa8e68098eeb475ebb52ea91e342a78852a677e
Igor Martins <iguinhoigor4@gmail.com>
2025-05-19 00:03:18 -0300

    feat: melhorar estilo e estrutura do componente Linha #25

--- front/src/components/Linha/Linha.tsx
+++ front/src/components/Linha/Linha.tsx
@@ -1,30 +1,33 @@
+import { FaPlus } from "react-icons/fa6";
+
 interface LinhaProps {
   numeroLinha: number;
 }
 
 export default function Linha({ numeroLinha }: LinhaProps) {
   return (
-    <div className="flex flex-col text-center m-4 p-4">
-      <button className="text-2xl font-bold p-6 bg-amber-700 w-full rounded-4xl">
+    <section className="flex flex-col text-center m-0 p-4 text-internoRotas-bege-areia">
+      <button className="text-2xl font-bold p-2 bg-amber-700 w-full rounded-2xl">
         Linha {numeroLinha}
       </button>
 
-      <div className="text-base flex justify-between items-center px-10">
+      <div className="text-base flex justify-between items-center px-10 bg-internoRotas-cinza-grafite mt-1 p-2 rounded-2xl font-bold">
         <div className="text-center">
-          <p>Anterior</p>
-          <p>20:00</p>
+          <p className="text-sm">Anterior</p>
+          <p className="text-xl">20:00</p>
         </div>
         <div className="text-center">
-          <p>Próximo</p>
-          <p>20:40</p>
+          <p className="text-sm">Próximo</p>
+          <p className="text-xl">20:40</p>
         </div>
       </div>
 
-      <div>
-        <button>Mais horários</button>
-      </div>
-      <div>itinerario</div>
-      <div>horarios</div>
-    </div>
+      <a className="mt-1 p-2 bg-transparent border-1 rounded-2xl flex items-center justify-center">
+        <button className="font-bold text-lg flex items-center gap-2">
+          <FaPlus  className="text-lg" />
+          <span>mais horários</span>
+        </button>
+      </a>
+    </section>
   );
 }
