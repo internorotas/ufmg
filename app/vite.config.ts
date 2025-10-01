a9ffaa43beaa1b9ab3c3a8776ddba2f4c4e73602
Igor Martins <iguinhoigor4@gmail.com>
2025-10-01 16:46:07 -0300

    fix: Corrigir erros configurações do vite para caminho da index #26

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), ghPages()],
-  base: '/ufmg/',
+  base: '/',
+  assetsInclude: ['**/*.geojson'],
 });
