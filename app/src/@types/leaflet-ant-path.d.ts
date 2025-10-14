e8e49fb90e7703ba60ad9d9476293a6633515858
Igor Martins <igor44@ufmg.br>
2025-10-14 00:53:32 -0300

    feat: adicionar definições de tipos para o módulo leaflet-ant-path #26

---
+++ app/src/@types/leaflet-ant-path.d.ts
@@ -0,0 +1,39 @@
+import * as L from 'leaflet';
+
+declare module 'leaflet' {
+  namespace Polyline {
+    class AntPath extends L.Polyline {
+      constructor(
+        latlngs: L.LatLngExpression[] | L.LatLngExpression[][],
+        options?: L.PolylineOptions & {
+          delay?: number;
+          dashArray?: number[];
+          weight?: number;
+          color?: string;
+          pulseColor?: string;
+          paused?: boolean;
+          reverse?: boolean;
+          hardwareAccelerated?: boolean;
+        }
+      );
+    }
+  }
+
+  function antPath(
+    latlngs: L.LatLngExpression[] | L.LatLngExpression[][],
+    options?: L.PolylineOptions & {
+      delay?: number;
+      dashArray?: number[];
+      weight?: number;
+      color?: string;
+      pulseColor?: string;
+      paused?: boolean;
+      reverse?: boolean;
+      hardwareAccelerated?: boolean;
+    }
+  ): Polyline.AntPath;
+}
+
+declare module 'leaflet-ant-path' {
+  export = L;
+}
