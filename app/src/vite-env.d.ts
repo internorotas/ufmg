d3e4b7a2a63f3de47f39e29099d1d30e1fde3261
Igor Martins <iguinhoigor4@gmail.com>
2026-04-17 20:58:08 -0300

    feat: adicionar definições de tipos para ImportMeta e ImportMetaEnv no arquivo vite-env.d.ts

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_VERSION: string;
+  readonly VITE_BUILD_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
