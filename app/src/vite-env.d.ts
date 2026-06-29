86e90de47115b56717274656c14cc5517010b033
Igor Martins <iguinhoigor4@gmail.com>
2026-06-29 11:27:04 -0300

    chore: adiciona VITE_TURNSTILE_SITE_KEY ao vite-env.d.ts

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -9,6 +9,7 @@ interface ImportMetaEnv {
   readonly VITE_SENTRY_DSN?: string;
   readonly VITE_TENANT_SLUG?: string;
   readonly VITE_VAPID_PUBLIC_KEY?: string;
+  readonly VITE_TURNSTILE_SITE_KEY?: string;
 }
 
 interface ImportMeta {
